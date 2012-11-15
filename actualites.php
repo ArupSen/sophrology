b124940a7a5afbb5a1c9be686220bc0a8cc26cf9
Arup Sen <arup@arupdesigns.co.uk>
2012-11-15 10:46:32 +0000

    added intro to soph book

--- actualites.php
+++ actualites.php
@@ -25,9 +25,16 @@ include_once ($root."/sl_includes_fr/sl_nav_fr.php"); //the top nav bar
 <p>Notre but est de d&eacute;velopper la sophrologie en Grande Bretagne où elle reste peu connue aujourd’hui. Nous proposons des cours de groupes et des ateliers dans diff&eacute;rents quartiers de Londres. Vous trouverez toutes les informations sur notre site internet : <a href="http://thesophrologynetwork.co.uk/" target="_blank">www.thesophrologynetwork.co.uk</a></p>
 <p>Le prochain atelier que j’anime &laquo;&nbsp;Discover Sophrology&nbsp;&raquo; aura lieu le Mardi 4 d&eacute;cembre 2012 &agrave; 19h au cabinet Body Logic pr&egrave;s de Battersea Park. Plus d’info sur <a href="http://thesophrologynetwork.co.uk/workshops/" target="_blank">http://thesophrologynetwork.co.uk/workshops/</a></p>
 <br />
+		<div class="photo">
+		<img src="/book_chene.jpg" alt="An Introduction to Sophrology" title="An Introduction to Sophrology" height="360" width="247" />
+		<p>Vient de sortir: le premier livre en anglais sur le sophrologie ! <br />"An Introduction to Sophrology" by Dr. P-A Ch&eacute;n&eacute;<br /><a href="contact.php">Pour le commander, merci de me contacter.</a>
+		</p>
+		</div>
+		<div class="clear">
+		</div>
 <img src="soph-institute.jpg" alt="the sophrology institute logo" />
 <p>Bient&ocirc;t &agrave; Londres, la premi&egrave;re &eacute;cole reconnue de sophrologie Cayc&eacute;dienne, The London Sophrology Institute.
-Plus d’info sur le site <a href="http://london-sophrology-institute.co.uk/" target="_blank">http://london-sophrology-institute.co.uk/</a></p>
+Plus d’info sur le site <a href="http://london-sophrology-institute.co.uk/" target="_blank">http://london-sophrology-institute.co.uk/</a></p><br />
 <p>Un article vient de para&icirc;tre dans le num&eacute;ro de L'Echo, le magazine des familles francophones de Londres: <a href="Sophro_L'Echo_Oct_12.pdf" target="_blank">Sophro_L'Echo_Oct_12.pdf</a></p>
 	</div><!-- end of column container -->
 	<div class="clear">
